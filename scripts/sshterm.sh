0c09872c20904d46c97b81772c650bea151bb026
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-09-21 15:17:21 +0300

    Login to remote server as default user

--- scripts/sshterm.sh
+++ scripts/sshterm.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 title=$(xdotool getwindowfocus getwindowname)
-server=$(echo $title | cut -d : -f 1)
+server=$(echo $title | cut -d @ -f 2 | cut -d : -f 1)
 path=$(echo $title | cut -d : -f 2)
 user=$(echo $title | cut -d @ -f 1)
 
 localhostname=$(hostname -s)
-localmachine="${user}@${localhostname}"
 fullpath=$(echo $path | sed "s/~/\/home\/$user/")
 
-if [[ $server == $localmachine ]]; then
+if [[ $server == $localhostname ]]; then
 	termite -d $fullpath
 else
 	termite --hold -e "ssh $server -t \"cd $path; bash --login\""
