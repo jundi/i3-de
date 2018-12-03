dc254ddd842047c0c1f5f17e315116cb52d2cc87
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-12-03 16:22:00 +0200

    retab

--- scripts/autorandr.sh
+++ scripts/autorandr.sh
@@ -7,9 +7,9 @@ primary_monitors="DP1-1 DP1-2 eDP1"
 # Function that prints list of connected monitors
 isconnected () {
     for i in $connected_monitors; do
-	if [[ "$i" == "$1" ]]; then
-	    return 0
-	fi
+        if [[ "$i" == "$1" ]]; then
+            return 0
+        fi
     done
     return 1
 }
@@ -28,16 +28,16 @@ randr_command="xrandr"
 primary_monitor=""
 for monitor in $primary_monitors; do
     if $(isconnected $monitor); then
-	if [[ -z $primary_monitor ]]; then
-	    primary_monitor=$monitor
-	    randr_command="$randr_command --output $monitor --primary --auto"
-	else
-	    randr_command="$randr_command --output $monitor --right-of $left_monitor --auto"
-	fi
-	left_monitor=$monitor
-	# wake up monitor
-	xrandr --output $monitor --off
-	xrandr --output $monitor --auto
+        if [[ -z $primary_monitor ]]; then
+            primary_monitor=$monitor
+            randr_command="$randr_command --output $monitor --primary --auto"
+        else
+            randr_command="$randr_command --output $monitor --right-of $left_monitor --auto"
+        fi
+        left_monitor=$monitor
+        # wake up monitor
+        xrandr --output $monitor --off
+        xrandr --output $monitor --auto
     else
         randr_command="$randr_command --output $monitor --off"
     fi
@@ -56,9 +56,9 @@ for monitor in $all_monitors; do
 
     if $(isconnected $monitor); then
         randr_command="$randr_command --output $monitor --right-of $left_monitor --auto"
-	left_monitor=$monitor
-	# wake up monitor
-	xrandr --output $monitor --auto
+        left_monitor=$monitor
+        # wake up monitor
+        xrandr --output $monitor --auto
     else
         randr_command="$randr_command --output $monitor --off"
     fi
@@ -74,4 +74,4 @@ $randr_command
 # Restart i3
 i3-msg restart
 
-# vim: sw=4
+# vim: sw=4:tabstop=4
