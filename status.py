22d1262419df53c3d2a0d9d038273b471c7a1d54
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2019-06-14 12:31:16 +0300

    Pylint/PEP8 fixes

--- status.py
+++ status.py
@@ -1,17 +1,15 @@
+"""Status bar for i3-wm"""
 # -*- coding: utf-8 -*-
-
-import subprocess
 
 from i3pystatus import Status
 
-status = Status(standalone=True)
-
+STATUS = Status(standalone=True)
 
 
 # Displays clock like this:
 # Tue 30 Jul 11:59:46 PM KW31
 #                          ^-- calendar week
-status.register("clock",
+STATUS.register("clock",
                 format="%F W%V %X",)
 
 
@@ -21,23 +19,26 @@ status.register("clock",
 # status.register("pulseaudio",
 #        format="VOL {volume}%",)
 
-status.register("mem",
+STATUS.register("mem",
                 format="MEM {used_mem:5.0f} MiB",)
 
-status.register("cpu_usage",
+STATUS.register("cpu_usage",
                 format="CPU {usage:3.0f}%",)
 
-status.register("network",
-                interface="enp0s20f0u1u2",
-                divisor=125000,
-                format_up="{interface}: ↑{bytes_sent:3.0f}Mbps / ↓{bytes_recv:3.0f}Mbps",)
+STATUS.register(
+    "network",
+    interface="enp0s20f0u1u2",
+    divisor=125000,
+    format_up="{interface}: ↑{bytes_sent:3.0f}Mbps / ↓{bytes_recv:3.0f}Mbps"
+)
 
-status.register("network",
-                interface="wlp2s0",
-                divisor=125000,
-                format_up="{essid} {quality:3.0f}%: ↑{bytes_sent:3.0f}Mbps / ↓{bytes_recv:3.0f}Mbps",)
+STATUS.register(
+    "network",
+    interface="wlp2s0",
+    divisor=125000,
+    format_up=("{essid} {quality:3.0f}%: ↑{bytes_sent:3.0f}Mbps / "
+               "↓{bytes_recv:3.0f}Mbps")
+)
 
 
-
-
-status.run()
+STATUS.run()
